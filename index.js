47673cc6db471a3078ea79269f418ea030694dc6
Anjali <raghav.anjali@gmail.com>
2020-07-20 18:17:03 +1200

    - removed server.js - index.js now logs lat long and descriptive details

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 
-const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
-console.log(distanceMsg);
-const currentText = document.querySelector('[gps-entity-place]').textContent;
-document.querySelector('[gps-entity-place]').setAttribute('value', currentText + ' +' + distanceMsg);
+
+window.onload = function () {
+    // async method to get the latlong. showLatLong callback method receives position object containing all details 
+    navigator.geolocation.getCurrentPosition(showLatLong);
+    getPositionOfAllBuildings();
+}
+
+
+function getPositionOfAllBuildings() {
+    // get all the buildings and get their distances from the browser. 
+    // all check the visibility flag and log on screen.
+    var allBuildings = [...document.querySelectorAll('a-text[gps-entity-place]')];
+    document.getElementById('noOfBuildings').innerHTML += allBuildings.length;
+
+    allBuildings.forEach(x => {
+        console.log(`${x.getAttribute('value')}: ${x.getAttribute('distancemsg')}: ${x.getAttribute('visible') !== ""}`);
+    });
+
+}
+
+
+function showLatLong(position) {
+    document.getElementById('lat').innerHTML += position.coords.latitude;
+    document.getElementById('long').innerHTML += position.coords.longitude;
+}
+
+
+/* 
+// register a custom event 
+AFRAME.registerComponent('markerhandler', {
+    init: function () {
+        this.el.sceneEl.addEventListener('gps-entity-place-loaded', (details) => {
+            // gps entity place loaded
+            console.log('Entity place loaded', details);
+        });
+    }
+});
+
+
+function onEntityLoaded(details){
+    console.log('Entity place loaded', details);
+} */
